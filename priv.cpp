3e705c25d6d6dca0ee17da8ed3580b7ac3e1b6b4
Marcial1234 <marcial.abrahantes@gmail.com>
2014-12-05 17:57:29 -0500

    Somewhat fixed priv.cpp, still needs further changes

--- priv.cpp
+++ priv.cpp
@@ -2,12 +2,41 @@
 #include <map>
 #include <iostream>
 #include <fstream>
-
-#include "priv.h"
+#include <map>
 
 using namespace std;
 
-void pullUserRecords() {
+class priv {
+private:
+	// these are ONLY input streams (they only read).
+	// Need output streams, and the file they're looking at needs to be declared later on 
+	ifstream fileUsers;
+	ifstream filePasswords;
+	ifstream filePrivs;
+	// sample output stream
+	ostream usersWritter;
+
+	map<string, string> users;
+	map<string, int> privileges;
+	string user;
+	string password;
+	//auth levels are 1) read 2) read and write 3) read, write, and execute
+	//some information will only be accessible at level 3, the admin level
+	int auth;
+
+public:
+	void pullUserRecords();
+	void pushUserRecords();
+	void login();
+	void setAdmin();
+	void setNewUser();
+	void setUserName();
+	void setPassword();
+	void setAccess();
+	int getAuth();
+};
+
+void priv::pullUserRecords() {
 	string tempstring;
 	string tempstring2;
 	int tempvalue;
@@ -23,14 +52,15 @@ void pullUserRecords() {
 	}
 }
 
-void pushUserRecords() {
+void priv::pushUserRecords() {
 
 
 }
 
-void login() {
+void priv::login() {
 	pullUserRecords();
-	if(users.Empty() == true) {
+
+	if(users.empty()) {
 	    // Question: Are we assuming the first user to setup the system will be an admin?
         setAdmin();
 	}
@@ -38,29 +68,33 @@ void login() {
 	cin >> user;
 	cout << "Please enter your password: " << endl;
 	cin >> password;
-	checkPassword();
-	for(int i = 0; i < 100; i++) {
-		if(strcmp(user, &users[i][0]) == 0 && strcmp(password, &users[i][1]) == 0) {
-			// Question: Why will priv be a two dimenstional array? 
-			priv = users[i][2];
-			return 1;
-		}
-	}
+	// THIS IS NOT MADE YET
+	// checkPassword();
+	// The following doesn't really follow how maps work... 
+	// Try with map.find() != map.end, ->first returns the key (user) and ->second returns value (password)
+	//for(int i = 0; i < 100; i++) {
+	//	if(user == users[i][0] && password == &users[i][1]) {
+	//		// Question: Why will priv be a two dimenstional array? 
+	//		// priv = users[i][2];
+	//		// return 1;
+	//	}
+	//}
+
 	cout << "Incorrect username/password combination. Please enter correct information." << endl;
 	cout << "Note: If you do not have a username and password, please contact your administrator for this system." << endl;
 	login();
 }
 
-void setAdmin() {
+void priv::setAdmin() {
 	cout << "Welcome to the Human Resources System (HRSys)!" << endl;
 	cout << "To get started, you will first need to create a username and password." << endl;
 	setUserName();
 	users[user] = password;
 	privileges[user] = 3;
-	filePrivs << 3;
+	// filePrivs << 3;
 }
 
-void setNewUser() {
+void priv::setNewUser() {
 	cout << "Welcome to the Human Resources System (HRSys)!" << endl;
 	cout << "To get started, you will first need to create a username and password." << endl;
 	setUserName();
@@ -69,17 +103,17 @@ void setNewUser() {
 	privileges[user] = auth;
 }
 
-void setUserName() {
+void priv::setUserName() {
 	cout << "Please enter you username: " << endl;
 	string tempUsername;
 	cin >> tempUsername;
 	cout << "Please confirm your username: " << endl;
 	string tempUsername2;
 	cin >> tempUsername2;
-	if(strcmp (tempUsername, tempUsername2) == 0) {
+	if(tempUsername == tempUsername2) {
 		cout << "User name set." << endl;
 		user = tempUsername;
-		fileUsers << tempUsername;
+		// fileUsers << tempUsername;
 		setPassword();
 	} 
 	else {
@@ -88,7 +122,7 @@ void setUserName() {
 	}
 }
 
-void setPassword() {
+void priv::setPassword() {
 	cout << "Please enter your password: " << endl;
 	string tempPassword;
 	cin >> tempPassword;
@@ -96,10 +130,10 @@ void setPassword() {
 	string tempPassword2;
 	cin >> tempPassword2;
 
-	if (strcmp(tempPassword, tempPassword2) == 0) {
+	if (tempPassword == tempPassword2) {
 		cout << "Password set!" << endl;
 		password = tempPassword;
-		filePasswords << tempPassword;
+		// filePasswords << tempPassword;
 	} 
 	else {
 		cout << "Error: Passwords do not match. Please retry." << endl;
@@ -107,8 +141,8 @@ void setPassword() {
 	}
 }
 
-void setAccess() {
-	users[user] password;
+void priv::setAccess() {
+	// users[user] password;
 	cout << "Set Privilege Level: " << endl;
 	cout << "\t" << "1. Read-Only" << endl;
 	cout << "\t" << "2. Read and Modify" << endl;
@@ -117,23 +151,23 @@ void setAccess() {
 	cin >> privlevel;
 	if (privlevel == 1) {
 		auth = 1;
-		filePrivs << 1;
+		// filePrivs << 1;
 		return;
 	}
 	if (privlevel == 2) {
 		auth = 2;
-		filePrivs << 2;
+		// filePrivs << 2;
 		return;
 	}
 	if (privlevel == 3) {
 		auth = 3;
-		filePrivs << 3;
+		// filePrivs << 3;
 		return;
 	}
 	cout << "Error: Invalid input. Please re-enter information." << endl;
 	setAccess();
 }
 
-int getAuth() {
+int priv::getAuth() {
 	return auth;
 }
