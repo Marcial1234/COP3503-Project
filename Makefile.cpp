aa7ca6845086ecebb532533db6d8c640a3683e83
DJGerts <zephyrdax@gmail.com>
2014-12-05 13:40:37 -0500

    Update make file and tweaked things
    Still doesn't work, reads syntax errors that don't make sense.
    Specifically, reads "#" as "invalid preprocessing directive" yet it is the standard form for commenting in makefiles. Will work on later have class now.

--- Makefile.cpp
+++ Makefile.cpp
@@ -1,44 +1,52 @@
-# HRSys makefile
-# Dax Gerts, Marcial Abrahantes, Ernie Chu, David Frick
+#HRSys makefile
 
-##set compiler
+#Compiler settings
 CC = g++
-CFLAGS = -g –Wall
+CFLAGS = -g -Wall
 
-##set first target entry
-all: HRSys
+default: HRSys
 
-##state object files to build executable
-main: database.o io.o priv.o crypt.o employee.o hash.o perform.o
-$(CC)	$(CFLAGS) –o main.cpp database.cpp io.cpp priv.cpp crypt.cpp employee.cpp hash.cpp perform.cpp
+#state object files to build executable
 
-##source files for database.o
-database.o: database.cpp database.h main.h 
-$(CC)	$(CFLAGS) –c database.cpp
+HRSys: main.cpp database.o priv.o crypt.o employee.o hash.o perf.o validation.o
+$(CC) $(CFLAGS) –o main.cpp database.o priv.o crypt.o employee.o hash.o perf.o validation.o
 
-##source files for io.o
-io.o: io.cpp io.h main.h
-$(CC)	$(CFLAGS) –c io.cpp
+#source files for validation.o
 
-##source files for priv.o
-priv.o: priv.cpp priv.o main.h
-$(CC)	$(CFLAGS) –c priv.cpp
+validation.o: valdiation.cpp validation.h database.h priv.h crypt.h hash.h perf.h employee.h validation.h
+	$(CC) $(CFLAGS) -c validation.cpp
 
-##source files for crypt.o
-crypt.o: crypt.cpp crypt.h main.h
-$(CC)	$(CFLAGS) –c crypt.cpp
+#source files for database.o
 
-##source files for employee.o
-employee.o: employee.cpp employee.h main.h
-$(CC)	$(CFLAGS) –c employee.cpp
+database.o: database.cpp database.h priv.h crypt.h hash.h perf.h employee.h validation.h
+	$(CC) $(CFLAGS) –c database.cpp 
 
-##source files for hash.o
-hash.o: hash.cpp hash.h main.h
-$(CC)	$(CFLAGS) –c hash.cpp
+#source files for priv.o
 
-##source files for perform.o
-perform.o: perform.cpp perform.h main.h
-$(CC)	$(CFLAGS) –c perform.cpp
+priv.o: priv.cpp priv.o
+	$(CC) $(CFLAGS) –c priv.cpp
+
+#source files for crypt.o
+
+crypt.o: crypt.cpp crypt.h 
+	$(CC) $(CFLAGS) –c crypt.cpp
+
+#source files for employee.o
+
+employee.o: employee.cpp employee.h
+	$(CC) $(CFLAGS) –c employee.cpp
+
+#source files for hash.o
+
+hash.o: hash.cpp hash.h
+	$(CC) $(CFLAGS) –c hash.cpp
+
+#source files for perform.o
+
+perf.o: perf.cpp perform.h
+	$(CC) $(CFLAGS) –c perf.cpp
+
 
 #to remove executable and allow for rebuild, type ‘make remove’
+
 remove:$(RM) main *.o *~
