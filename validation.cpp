595311ba26ac885b9adc131505b75d10c66b847d
DJGerts <zephyrdax@gmail.com>
2014-12-06 14:46:45 -0500

    Update validation.cpp

--- validation.cpp
+++ validation.cpp
@@ -1,3 +1,10 @@
+#include "validation.h"
+#include "database.h"
+#include "employee.h"
+#include "priv.h"
+#include "hash.h"
+#include "crypt.h"
+#include "perf.h"
 #include "generic dependencies.h"
 
 // Possible list of enums to link back and forth to functions
