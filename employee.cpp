89147205fc005431334147897fcd811874b11903
FrickProgrammer <davidfrick@ufl.edu>
2014-12-07 17:11:28 -0500

    Update employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -34,28 +34,7 @@ Employee::Employee() {
 
 	cout << "Note: Make sure to update employee information on a regular basis." << endl;
 }
-friend ostream Employee::&operator<<(ostream &out, Employee worker)
-{
-     out << worker.getName();
-     out << worker.getOccupation();
-     if(worker.getRaise())
-     {
-         worker.getRaiseValue();
-     }
-     if(worker.getInsurance())
-     {
-         worker.getInsurancePlan();
-     }
-	 out << worker.getComment();
-	 out << worker.getAge();
-	 out << worker.getSalary();
-	 out << worker.getHashSSN();
-	 out << worker.getTimeEmployed();
-	 out << worker.getHoursWorkedWeek();
-	 out << worker.getHoursWorkedMonth();
-	 out << worker.getPerformanceReport();
-     return out;
-}
+
 
 // Again the issue of 'value.length', defaulted to string in this case since no prior type was present.
 int Employee::infoCheck(string type, string value) {
@@ -384,3 +363,27 @@ void Employee::printOccuption() {
 
 //other variable management functions
 void payEmployee();
+
+//Overloaded << operator to print the details of a Employee object
+friend ostream &operator<<(ostream &out, Employee worker)
+{
+     out << worker.getName();
+     out << worker.getOccupation();
+     if(worker.getRaise())
+     {
+         worker.getRaiseValue();
+     }
+     if(worker.getInsurance())
+     {
+         worker.getInsurancePlan();
+     }
+	 out << worker.getComment();
+	 out << worker.getAge();
+	 out << worker.getSalary();
+	 out << worker.getHashSSN();
+	 out << worker.getTimeEmployed();
+	 out << worker.getHoursWorkedWeek();
+	 out << worker.getHoursWorkedMonth();
+	 out << worker.getPerformanceReport();
+     return out;
+}
