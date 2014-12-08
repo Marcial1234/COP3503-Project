33c219fddc54862a2cdb46a1ae90b02aa6706b5d
e-chu <engineer.chu@gmail.com>
2014-12-07 22:52:23 -0500

    Updated "perf.cpp"
    Please see the comments on what needs to be done.

--- perf.cpp
+++ perf.cpp
@@ -1,19 +1,33 @@
-#pragma once
 /*
     File: perf.cpp
     Author: Marcial Abrahantes, Ernie Chu, David Frick, Dax Gerts
     Submitted: December 5, 2014
 */
 
+/* Todo: Find some way to interface with "employee.cpp". 
+         A way to implement this would be to print the contents of the "employeeFile.txt" 
+         to console. Please assist by all means. This should be relatively simple.
+
+    Todo: setPerformanceReport() might actually be handled in "employee.cpp". 
+          From my understanding, it should call the main() in perf.cpp, which 
+          calls the EmployeeReport constructor. Please clarify on this.
+
+    Note: I'm planning on implmenting some very rudimentary validation checking. 
+          For example, the module might ask the user to try again if a field was 
+          left blank. Marcial, I'd appreciate any insight you have on validation.
+*/
+
 #include "perf.h"
+#include "gendepend.h"
 
 using namespace std;
 
 EmployeeReport::EmployeeReport(string employeeFile, string reportNumber) {
     ofstream toFile(employeeFile,ios_base::app);
-    toFile << "\n" << reportNumber;
-
-    cout << "Performance Report: " << "\n" << endl;
+    toFile << "\n" << reportNumber << "\n";
+    
+    cout << "\nWelcome to the HRS Employee Report and Aquisition Module!" << endl;
+    cout << "Please enter the information requested and press [Enter] after finishing." << "\n" << endl;
 
 	cout << "Employee Name: ";
 	getline(cin, name);
@@ -25,7 +39,7 @@ EmployeeReport::EmployeeReport(string employeeFile, string reportNumber) {
 
 	cout << "Leadership Ability: ";
 	getline(cin, leadershipAbility);
-    toFile << "Leadership Ability" << leadershipAbility << endl;
+    toFile << "Leadership Ability: " << leadershipAbility << endl;
 
 	cout << "Quality of Work: ";
 	getline(cin, workQuality);
@@ -49,17 +63,29 @@ EmployeeReport::EmployeeReport(string employeeFile, string reportNumber) {
 
 	cout << "Additional Comments: ";
     getline(cin, additionalComments);
-    toFile << "Additional Comments: " << additionalComments << "\n" << endl;
+    toFile << "Additional Comments: " << additionalComments << endl;
     
     toFile.close();
 }
-/*
-// Example use
+
+// Some testing.
 int main() {
-    string employeeFile = "employeeFile.txt";
-    string reportNumber = "report1.txt";
-    EmployeeReport report = *(new EmployeeReport(employeeFile, reportNumber));
+
+  string continueOption = "1";
+  int numberOfReports = 0;
+
+    while (continueOption == "1") {
+        
+        string employeeFile = "employeeFile.txt";
+        string reportNumber = "Employee Report";
+        EmployeeReport report = *(new EmployeeReport(employeeFile, reportNumber));
+        numberOfReports++;
+
+        cout << "\nDo you wish to add another employee report?" << endl;
+        cout << "If so, enter '1' (no quotes). Otherwise, enter anything else." << endl;
+        getline(cin, continueOption);
+        cout << numberOfReports << endl;
+    }
 
 	return 0;
 }
-*/
--- perf.h
+++ perf.h
@@ -1,9 +1,11 @@
-#pragma once
 /*
     File: perf.h
     Author: Marcial Abrahantes, Ernie Chu, David Frick, Dax Gerts
     Submitted: December 5, 2014
 */
+
+#ifndef PERF_H
+#define PERF_H
 
 #include "gendepend.h"
 
@@ -22,5 +24,8 @@ private:
 
 public:
 	EmployeeReport(std::string employeeFile, std::string reportNumber);
+    // Ain't needed. Should be handled by validation.
 	int checkUserAuthorization();
 };
+
+#endif
