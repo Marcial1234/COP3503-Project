e05bd32b9b212206ca51f2ec11785a14a21b7b95
DJGerts <zephyrdax@gmail.com>
2014-12-08 15:34:15 -0500

    revolutionary database reduction, SQL is dead

--- database.cpp
+++ database.cpp
@@ -9,75 +9,26 @@ using namespace std;
 
 // The database should be a map
 
-bool Database::loadEmployee(string filename) {
-
-    if (ifstream(filename)) {
-        addEmployee(*(new Employee()));
-        return true;
-    }
-    else
-        return false;
+void Database::addEmployee(string worker) {
+	ofstream employeesFile ("employees.txt",ios_base::app);
+	string tempstring = worker += ".txt";
+	employeesFile << tempstring << endl;
+    return;
 }
 
-void Database::addEmployee(Employee worker) {
-
-    records.push_back(worker);
-}
-
-// TODO
-bool Database::deleteEmployee(string filename) {
-	records.delete()
-   /* if (remove (filename) != 0) {
+bool Database::deleteEmployee(string worker) {
+	string tempstring = worker += ".txt";
+    if (remove (tempstring) != 0) {
         perror("Error deleting file");
         return false;
-    } else {
-        return true;
-    }*/
+    } 
+    fstream openFile ("employees.txt");
+    string line1;
+    if(openFile.is_open()) {
+		while(getline(openFile,line1) {
+			if(line == tempstring) {
+				openFile2 << "employee removed";
+			}
+	}
     return false;
 }
-
-void Database::printRecords(Employee worker) {
-	worker.printEmployeeRecords();
-}
-
-void Database::printAllRecords(vector<Employee> employee_records) {
-
-	for (int i = 0; i != employee_records.size(); i++ ) {
-		printRecords(employee_records.at(i));
-	}
-}
-
-bool Database::newFile(string filename) {
-    ifstream stream(filename);
-    if (stream) {
-        cout << "File already exists." << endl;
-        return false;
-    }
-
-    ofstream file(filename);
-
-    if (!file) {
-        cout << "File could not be created" << endl;
-        return false;
-    }
-    else
-        return true;
-}
-
-void Database::printDirectory(string filepath)
-{
-    /*
-    DIR *dpdf;
-    struct dirent *epdf;
-    dpdf = opendir("C:/Users/David/Desktop/exemple2.txt");
-    if(dpdf != NULL)
-    {
-    	while (epdf = readdir(dpdf)){
-      	printf("Filename: %s",epdf->d_name);
-      // std::out << epdf->d_name << std::endl;
-   	}
-    }
-    */
-}
-
-//#include <dirent.h>
