110a59b50142365d844386e4101d7d39838ddaa8
e-chu <engineer.chu@gmail.com>
2014-12-07 20:27:39 -0500

    Refactored "database.cpp"
    Minor formatting changes. I made the termination conditions more
    explicit by adding "else" branches.

--- database.cpp
+++ database.cpp
@@ -4,53 +4,65 @@
 
 using namespace std;
 
-// No idea of what 'add_employee' is. File manipulation is odd later on and should be done, rather
-// All the mods should be done on the local maps
+// No idea of what 'add_employee' is. File manipulation is odd later on and should be done, rather.
+// All the mods should be done on the local maps.
+// Ernie: What does "mods being done on local maps" mean?
 
 bool Database::loadEmployee() {
+
     if (ifstream(filename)) {
         add_employee(new Employee());
         return true;
     }
-    return false;
+
+    else 
+        return false;
 }
 
 void Database::addEmployee(Employee worker) {
+
     records.push_back(worker);
 }
 
 bool Database::deleteEmployee(Employee worker) {
-    if (ifstream(worker.reports)) 
-    {
-        if(remove(worker.reports) == 0 ))
-        {
+
+    if (ifstream(worker.reports)) {
+
+        if(remove(worker.reports) == 0 )) {
         	return true;
         }
     }
-    return false;
+
+    else 
+        return false;
 }
 
 void Database::printRecord(Employee worker) {
+
     cout << worker << endl;
 }
 
-void Database::printAllRecords(vector<Employee> employee_records)
-{
-	for(int i = 0; i != employee_records.size(); i++ )
-	{
+void Database::printAllRecords(vector<Employee> employee_records) {
+
+	for (int i = 0; i != employee_records.size(); i++ ) {
 		cout << employee_records.at(i); << endl;
 	}
 }
 
 bool Database::newFile(string filename) {
+
     if (ifstream(filename)) {
         cout << "File already exists." << endl;
         return false;
     }
+
     ofstream file(filename);
+
     if (!file) {
         cout << "File could not be created" << endl;
         return false;
     }
-    return true;
+
+    else 
+        return true;
 }
