194870f783b8ff36607d1f914b274f73c4dc9e40
Marcial1234 <marcial.abrahantes@gmail.com>
2014-12-08 21:35:22 -0500

    Added Validation documentation

--- validation.cpp
+++ validation.cpp
@@ -4,50 +4,24 @@
 
 using namespace std;
 
-// Marcial: Make these methods static later.
-
-/*
-	Documentation/How to Use:
-		
-		printMainMenu(){}:
-		Prints the "Main Menu" based on a local definition of 'auth'
-
-		printGenericMenu(string, vector<stirng>){}
-		Menu generator. Generates the following:
-		[action description/instructions]
-			[i]: vector[i]
-			...
-		Example:
-		printGenericMenu("Do you know your name?", { "No", "Yes" });
-		Do you know your name?
-			0. No
-			1. Yes
-
-		RecursivelyValidate(maxNumber){}
-		Returns the input of a number ONLY LOWER than the passed max/upper limit.
-
-		Did not combine them (as in Recur.Val. inside Gener.Menu). MIGHT do so later.
-		Wanted flexibility.
- */
-
-void ClearCin()
+static void ClearCin()
 {
 	cin.clear();
 	cin.ignore(1024, '\n');
 }
 
-void SpitOutStupidity() {
+static void SpitOutStupidity() {
 	cout << "\nSorry, that was an invalid input, try again.\n" << endl;
 	ClearCin();
 }
 
 // Numeric validation
-bool ValidateRange(long long int input, long long int  max) {
+static bool ValidateRange(long long int input, long long int  max) {
 	// Use of Discrete right here
 	return max > input && input >= 0;
 }
 
-long long int RecursivelyValidate(long long int max) {
+static long long int RecursivelyValidate(long long int max) {
 	long long int input = 0;
 
 	if ((cin >> input) && ValidateRange(input, max))
@@ -60,7 +34,7 @@ long long int RecursivelyValidate(long long int max) {
 }
 
 // String Validation
-string RecursivelyValidate() {
+static string RecursivelyValidate() {
 	string input = "";
 	ClearCin();
 
@@ -73,7 +47,7 @@ string RecursivelyValidate() {
 	}
 }
 
-void printGenericMenu(string placeHolder, vector<string> &v) {
+static void printGenericMenu(string placeHolder, vector<string> &v) {
 	cout << placeHolder << endl;
 
 	// Printing custom Menu based on vector parameters type
